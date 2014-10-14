8688f3768452cb1393bc8260c99b06ac3fcddb09
Patrizia Zehnder <patriziasusanna.zehnder@students.bfh.ch>
2014-10-14 13:44:14 +0200

    blabla

--- StationClient/src/example/HelloWorldClient.java
+++ StationClient/src/example/HelloWorldClient.java
@@ -1,16 +1,21 @@
-package example;/**
+package example;
+
+import swissindex.*;
+/**
  * Created by Johannes on 14.10.14.
  */
 public class HelloWorldClient {
   public static void main(String[] argv) {
       try {
           Ws_Pharma_V101Locator locator = new Ws_Pharma_V101Locator();
-          Ws_Pharma_V101Soap_PortType service = locator.get();
-          // If authorization is required
-          //((Ws_Pharma_V101Soap12Stub)service).setUsername("user3");
-          //((Ws_Pharma_V101Soap12Stub)service).setPassword("pass3");
-          // invoke business method
-          service.businessMethod();
+                    Ws_Pharma_V101Soap_PortType service = locator.getws_Pharma_V101Soap();
+                    final PHARMA de = service.getByGTIN("7680340960153", "de");
+
+                    for (PHARMAITEM pharmaitem : de.getITEM()) {
+                            PHARMAITEMCOMP comp = pharmaitem.getCOMP();
+
+                            System.out.println("Firma: " + comp.getNAME() + ", GLN: " + comp.getGLN());
+                        }
       } catch (javax.xml.rpc.ServiceException ex) {
           ex.printStackTrace();
       } catch (java.rmi.RemoteException ex) {
