a8b9f252c645e7f7b37e206baed90b0dc3330f63
Johannes Gnägi <johannes3004@gmail.com>
2014-10-14 10:44:37 +0200

    added swissindex webservice to client

--- StationClient/src/example/HelloWorldClient.java
+++ StationClient/src/example/HelloWorldClient.java
@@ -1,10 +1,21 @@
-package example;/**
+package example;
+
+import swissindex.PHARMA;
+import swissindex.WsPharmaV101;
+import swissindex.WsPharmaV101Soap;
+
+import java.util.List;
+
+/**
  * Created by Johannes on 13.10.14.
  */
 public class HelloWorldClient {
   public static void main(String[] argv) {
-      swissindex.WsPharmaV101Soap service = new WsPharmaV101().getPort();
-      //invoke business method
-      service.downloadAll();  
+      swissindex.WsPharmaV101 pharma = new WsPharmaV101();
+      WsPharmaV101Soap wsPharmaV101Soap = pharma.getWsPharmaV101Soap();
+      PHARMA p = wsPharmaV101Soap.downloadAll("de");
+
+      List<PHARMA.ITEM> item = p.getITEM();
+      System.out.println("Hello");
   }
 }
