3d625ec199df81b47b97de8ca6468cbb86a5e041
Production <hirsp4@bfh.ch>
2015-05-28 14:41:39 +0200

    update webservice mediprep

---
+++ MediPrep/resources/webservice/erp/GetItemByIdentifierResponse.java
@@ -0,0 +1,62 @@
+
+package webservice.erp;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für getItemByIdentifierResponse complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType name="getItemByIdentifierResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://service/}item" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getItemByIdentifierResponse", propOrder = {
+    "_return"
+})
+public class GetItemByIdentifierResponse {
+
+    @XmlElement(name = "return")
+    protected Item _return;
+
+    /**
+     * Ruft den Wert der return-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Item }
+     *     
+     */
+    public Item getReturn() {
+        return _return;
+    }
+
+    /**
+     * Legt den Wert der return-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Item }
+     *     
+     */
+    public void setReturn(Item value) {
+        this._return = value;
+    }
+
+}
